d1a4e6b2e7a65066a41fd774bd6c56205b9ad3d6
Renan <renancastrotut@gmail.com>
2022-03-06 18:08:45 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import Logo from './Image/Logo.png';
+import BotaoResultado from './Image/BotaoResultado.png';
+import BotaoTrocar from './Image/BotaoTrocar.png';
+
+
 import './App.css';
 
 function App() {
+  const [trocar, setTrocar] = useState(true);
+  const [numero, setNumero] = useState();
+  const [resultado, setResultado] = useState();
+
+  function ConverterBinToDec(){
+    setResultado(parseInt(numero,2));
+  }
+
+  function ConverterDecToBin(){
+    setResultado((numero >>> 0).toString(2));
+  }
+
+  const handleClickTrocar = ()=>{
+    trocar ? setTrocar(false) : setTrocar(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={Logo} alt ="Logo" className="imagem"/>
+
+      <div >
+        <div className="divInput">
+          <label className="labelStyle">
+            {trocar ? "Número binário" : "Número decimal"}
+          </label>
+          <input 
+            className="input" 
+            onChange={(e)=>{setNumero(e.target.value)}}
+          />
+        </div>
+
+        <div className="divBotoes">
+          <button 
+            className="botao" 
+            onClick={trocar ? ConverterBinToDec : ConverterDecToBin}>
+            <img src={BotaoResultado} alt ="Botao resultado" className="imagemBotao"/>  
+          </button> 
+          <p>Converter</p>
+        </div>
+
+        <div className="divBotoes">
+          <button className="botao" onClick={handleClickTrocar}>
+            <img src={BotaoTrocar} alt ="Botao trocar" className="imagemBotao"/>  
+          </button> 
+          <p>Trocar</p>
+        </div>
+      </div>
+
+      <p className="labelResultadoStyle">
+        {trocar ? "Número decimal" : "Número binário"}
+      </p>
+      <p className="resultado">{resultado}</p>
     </div>
   );
 }
